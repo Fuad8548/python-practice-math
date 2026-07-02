cf4223cdc21980e4104c5ea6f1d141e9d1986a63
Fuad8548 <alfuad8548@gmail.com>
2026-07-02 09:12:27 +0000

    equal distribution math

--- python-prctc-math.py
+++ python-prctc-math.py
@@ -1,5 +1,16 @@
 import math
 
-print(math.sqrt(114921))
+# print(math.sqrt(114921))
 
+# when 117 chocolates eqully distributed to x students, we are left by x-9 choco. x=?
+n = 126  # 117 + 9
 
+solutions = []
+for x in range(1, n + 1):
+    if n % x == 0 and x > 9:          # x must divide 126 and be > 9
+        q = (n // x) - 1              # chocolates per student
+        r = x - 9                     # remainder
+        solutions.append((x, q, r))
+
+for x, q, r in solutions:
+    print(f"x = {x} -> each student gets {q}, remainder = {r}")
